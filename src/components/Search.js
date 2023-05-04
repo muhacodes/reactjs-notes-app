786ab1d2dc8b2d01e4ea1af6f2e6c00577879183
muhacodes <muha.oq3@gmail.com>
2023-05-04 21:50:19 +0100

    reactjs notes app

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
-import {MdSearch} from 'react-icons/md';
+import React, { useState } from "react";
+import { MdSearch } from "react-icons/md";
 
-function Search({onSearch}) {
-
-    const [search, setSearch] = useState('');
-    const searchNote = (event) => {
-        setSearch(event.target.value);
-        onSearch(search);
-    }
+function Search({ onSearch }) {
+  const [search, setSearch] = useState("");
+  const searchNote = (event) => {
+    setSearch(event.target.value);
+    onSearch(search);
+  };
   return (
-    <div className='search'>
-        <MdSearch className='search-icons' size='1.5em' />
-        <input value={search} onChange={searchNote} type='text' placeholder='Type to search' />
+    <div className="search">
+      <MdSearch className="search-icons" size="1.5em" />
+      <input
+        value={search}
+        onChange={searchNote}
+        type="text"
+        placeholder="Type to search"
+      />
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
